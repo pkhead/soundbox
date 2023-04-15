306b666d111de7d63b96a4142194c8dfe6b86f35
pkhead <chromosoze@gmail.com>
2023-04-14 23:01:13 -0500

    remove build output from repo

--- preload.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-window.addEventListener("DOMContentLoaded", () => {
-    console.log("preload");
-});
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHJlbG9hZC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInNyYy9wcmVsb2FkLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSxNQUFNLENBQUMsZ0JBQWdCLENBQUMsa0JBQWtCLEVBQUUsR0FBRyxFQUFFO0lBQzdDLE9BQU8sQ0FBQyxHQUFHLENBQUMsU0FBUyxDQUFDLENBQUM7QUFDM0IsQ0FBQyxDQUFDLENBQUMifQ==
