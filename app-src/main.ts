b0e087e1bea2e32a4252de1b94e3839a3535f659
pkhead <chromosoze@gmail.com>
2023-04-14 20:28:58 -0500

    rename seq editor to track editor

--- app-src/main.ts
+++ app-src/main.ts
@@ -1,10 +1,10 @@
-import { SequenceEditor } from "./seq-editor";
+import { TrackEditor } from "./track-editor";
 console.log("Hello, world!");
 
-let sequenceEditor; {
+let trackEditor; {
     let canvas = document.getElementById("song-editor");
     if (!canvas || !(canvas instanceof HTMLCanvasElement)) {
         throw new Error("could not find #song-editor")
     }
-    sequenceEditor = new SequenceEditor(canvas);
+    trackEditor = new TrackEditor(canvas);
 }
