2d14908f9c515c0b6128892706697866b1a529ea
pkhead <chromosoze@gmail.com>
2023-05-27 11:28:11 -0500

    implement gain module

--- src/modules/gain.cpp
+++ src/modules/gain.cpp
@@ -14,13 +14,16 @@ GainModule::GainModule() : ModuleBase(true) {
 }
 
 void GainModule::process(float** inputs, float* output, size_t num_inputs, size_t buffer_size, int sample_rate, int channel_count) {
+    float factor = powf(10.0f, gain / 10.0f);
+
     for (size_t i = 0; i < buffer_size; i += channel_count) {
-        output[i] = inputs[0][i];
-        output[i+1] = inputs[0][i+1];
+        output[i] = inputs[0][i] * factor;
+        output[i+1] = inputs[0][i+1] * factor;
     }
 }
 
 void GainModule::_interface_proc() {
     ImGui::SetNextItemWidth(200.0f);
-    ImGui::SliderFloat("###gain", &gain, -10.0f, 10.0f);
+    ImGui::SliderFloat("###gain", &gain, -50.0f, 50.0f, "%.3f dB");
+    if (ImGui::IsItemClicked(ImGuiMouseButton_Middle)) gain = 0.0f;
 }
