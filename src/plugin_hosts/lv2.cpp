62fe3fb8043c00a2330db29237d2f34849ce214e
pkhead <chromosoze@gmail.com>
2023-06-25 18:31:34 -0500

    fill out lv2 plugin data

--- src/plugin_hosts/lv2.cpp
+++ src/plugin_hosts/lv2.cpp
@@ -1,7 +1,12 @@
+#include <cassert>
+#include <cstring>
+#include <iostream>
+
 #include "lv2.h"
+
 using namespace plugins;
 
-static LilvWorld* LILV_WORLD;
+static LilvWorld* LILV_WORLD = nullptr;
 
 void Lv2Plugin::lilv_init() {
     LILV_WORLD = lilv_world_new();
@@ -30,6 +35,8 @@ const char* Lv2Plugin::get_standard_paths()
 
 void Lv2Plugin::scan_plugins(const std::vector<std::string> &paths, std::vector<PluginData> &data_out)
 {
+    assert(LILV_WORLD);
+
     // create path string to be parsed by lilv
     const char delim =
 #ifdef _WIN32
@@ -44,7 +51,48 @@ void Lv2Plugin::scan_plugins(const std::vector<std::string> &paths, std::vector<
         path_str += paths[i];
     }
 
-    LilvNode* lv2_path = lilv_new_file_uri(LILV_WORLD, NULL, path_str.c_str());
+    LilvNode* lv2_path = lilv_new_file_uri(LILV_WORLD, NULL, "/usr/lib/lv2");
     lilv_world_set_option(LILV_WORLD, LILV_OPTION_LV2_PATH, lv2_path);
     lilv_node_free(lv2_path);
+
+    lilv_world_load_all(LILV_WORLD);
+
+    const LilvPlugins* plugins = lilv_world_get_all_plugins(LILV_WORLD);
+
+    // list plugins
+    LILV_FOREACH (plugins, i, plugins)
+    {
+        const LilvPlugin* plug = lilv_plugins_get(plugins, i);
+
+        PluginData data;
+        data.type = PluginType::Lv2;
+
+        const LilvNode* uri_node = lilv_plugin_get_uri(plug);
+        data.id = "plugin.lv2:";
+        data.id += lilv_node_as_uri(uri_node);
+
+        const LilvNode* lib_uri = lilv_plugin_get_library_uri(plug);
+        char* file_path = lilv_node_get_path(lib_uri, NULL);
+        data.file_path = file_path;
+
+        LilvNode* name = lilv_plugin_get_name(plug);
+        if (name) {
+            data.name = lilv_node_as_string(name);
+            lilv_node_free(name);
+        } else {
+            data.name = lilv_node_as_uri(uri_node);
+        }
+        
+        LilvNode* author = lilv_plugin_get_author_name(plug);
+        if (author) {
+            data.author = lilv_node_as_string(author);
+            lilv_node_free(author);
+        }
+
+        const LilvPluginClass* plug_class = lilv_plugin_get_class(plug);
+        const char* class_uri = lilv_node_as_uri ( lilv_plugin_class_get_uri(plug_class) );
+        data.is_instrument = strcmp(class_uri, "http://lv2plug.in/ns/lv2core#InstrumentPlugin") == 0;
+
+        data_out.push_back(data);
+    }
 }
