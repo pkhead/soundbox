512f0cebd90cf5de1a40981c3b97b94f0c59b6f9
pkhead <chromosoze@gmail.com>
2023-07-18 16:00:50 -0500

    use MessageQueue in LimiterModule

--- src/modules/limiter.cpp
+++ src/modules/limiter.cpp
@@ -7,15 +7,33 @@
 
 using namespace audiomod;
 
-LimiterModule::LimiterModule(DestinationModule& dest) : ModuleBase(dest, true) {
+LimiterModule::LimiterModule(DestinationModule& dest)
+:   ModuleBase(dest, true),
+    process_queue(sizeof(message_t), 8),
+    ui_queue(sizeof(message_t), 8)
+{
     id = "effect.limiter";
     name = "Limiter";
+    
+    // initialize states
+    module_state* states[2] = { &process_state, &ui_state };
+    analytics_t* analytics[2] = { &ui_analytics, &process_analytics };
 
-    _input_gain = 0.0f;
-    _output_gain = 0.0f;
-    _cutoff = -1.0f;
-    _attack = 10.0f;
-    _decay = 500.0f;
+    for (int i = 0; i < 2; i++) {
+        module_state* state = states[i];
+        state->input_gain = 0.0f;
+        state->output_gain = 0.0f;
+        state->attack = 10.0f;
+        state->decay = 500.0f;
+        state->threshold = -0.5f;
+
+        analytics_t* a = analytics[i];
+        a->in_volume[0] = 0.0f;
+        a->in_volume[1] = 0.0f;
+        a->out_volume[0] = 0.0f;
+        a->out_volume[1] = 0.0f;
+    }
+
     _limit[0] = 0.0f;
     _limit[1] = 0.0f;
 
@@ -32,20 +50,12 @@ LimiterModule::LimiterModule(DestinationModule& dest) : ModuleBase(dest, true) {
 void LimiterModule::save_state(std::ostream& ostream)
 {
     push_bytes<uint8_t>(ostream, 0); // version
-    
-    lock.lock();
-    float input_gain = _input_gain;
-    float output_gain = _output_gain;
-    float cutoff = _cutoff;
-    float decay = _decay;
-    float attack = _attack;
-    lock.unlock();
 
-    push_bytes<float>(ostream, input_gain);
-    push_bytes<float>(ostream, output_gain);
-    push_bytes<float>(ostream, cutoff);
-    push_bytes<float>(ostream, decay);
-    push_bytes<float>(ostream, attack);
+    push_bytes<float>(ostream, ui_state.input_gain);
+    push_bytes<float>(ostream, ui_state.output_gain);
+    push_bytes<float>(ostream, ui_state.threshold);
+    push_bytes<float>(ostream, ui_state.decay);
+    push_bytes<float>(ostream, ui_state.attack);
 }
 
 bool LimiterModule::load_state(std::istream& istream, size_t size)
@@ -55,35 +65,61 @@ bool LimiterModule::load_state(std::istream& istream, size_t size)
 
     float input_gain = pull_bytesr<float>(istream);
     float output_gain = pull_bytesr<float>(istream);
-    float cutoff = pull_bytesr<float>(istream);
+    float threshold = pull_bytesr<float>(istream);
     float decay = pull_bytesr<float>(istream);
     float attack = pull_bytesr<float>(istream);
 
-    lock.lock();
-    _input_gain = input_gain;
-    _output_gain = output_gain;
-    _cutoff = cutoff;
-    _decay = decay;
-    _attack = attack;
-    lock.unlock();
-    
+    ui_state.input_gain = input_gain;
+    ui_state.output_gain = output_gain;
+    ui_state.threshold = threshold;
+    ui_state.decay = decay;
+    ui_state.attack = attack;
+
+    // send state to processing
+    message_t new_msg;
+
+    new_msg.type = message_t::ModuleState;
+    new_msg.mod_state = ui_state;
+    if (process_queue.post(&new_msg, sizeof(new_msg))) {
+        dbg("WARNING: LimiterModule process queue is full!\n");
+    }
+
     return true;
 }
 
 void LimiterModule::process(float** inputs, float* output, size_t num_inputs, size_t buffer_size, int sample_rate, int channel_count) {
-    lock.lock();
-    float input_gain = _input_gain;
-    float output_gain = _output_gain;
-    float cutoff = db_to_mult(_cutoff);
-    float decay = _decay;
-    float attack = _attack;
-    lock.unlock();
+    // read messages sent from ui thread
+    while (true)
+    {
+        MessageQueue::read_handle_t handle = process_queue.read();
+        if (!handle) break;
 
-    float in_factor = db_to_mult(input_gain);
-    float out_factor = db_to_mult(output_gain);
+        assert(handle.size() == sizeof(message_t));
+        message_t* msg = (message_t*) handle.data();
 
-    float a = powf(0.01f, 1.0f / (attack * _dest.sample_rate * 0.001f));
-    float r = powf(0.01f, 1.0f / (decay * _dest.sample_rate * 0.001f));
+        // update module state
+        if (msg->type == message_t::ModuleState) {
+            process_state = msg->mod_state;
+        }
+
+        // send analytics to ui thread
+        else if (msg->type == message_t::RequestAnalytics) {
+            message_t new_msg;
+            new_msg.type = message_t::ReceiveAnalytics;
+            new_msg.analytics = process_analytics;
+
+            ui_queue.post(&new_msg, sizeof(new_msg));
+        }
+    }
+
+    module_state& state = process_state;
+
+    float in_factor = db_to_mult(state.input_gain);
+    float out_factor = db_to_mult(state.output_gain);
+    float threshold = db_to_mult(state.threshold);
+
+    float a = powf(0.01f, 1.0f / (state.attack * _dest.sample_rate * 0.001f));
+    float r = powf(0.01f, 1.0f / (state.decay * _dest.sample_rate * 0.001f));
     
     for (size_t i = 0; i < buffer_size; i += channel_count) {
         // receive inputs
@@ -111,8 +147,8 @@ void LimiterModule::process(float** inputs, float* output, size_t num_inputs, si
                 limit = r * (limit - v) + v;
 
             // if limit surpasses the threshold, perform limiting
-            if (limit > cutoff) 
-                output[i+c] = (output[i+c] / limit) * cutoff;
+            if (limit > threshold) 
+                output[i+c] = (output[i+c] / limit) * threshold;
 
             output[i+c] *= out_factor; // output gain control
 
@@ -139,28 +175,35 @@ void LimiterModule::process(float** inputs, float* output, size_t num_inputs, si
     }
 
     float _limit_out_buf[2];
-    _limit_out_buf[0] = max(_limit[0], cutoff);
-    _limit_out_buf[1] = max(_limit[1], cutoff);
+    _limit_out_buf[0] = max(_limit[0], state.threshold);
+    _limit_out_buf[1] = max(_limit[1], state.threshold);
 
-    lock.lock();
-    memcpy(_in_volume, inv, 2 * sizeof(float));
-    memcpy(_out_volume, outv, 2 * sizeof(float));
-    lock.unlock();
+    memcpy(process_analytics.in_volume, inv, 2 * sizeof(float));
+    memcpy(process_analytics.out_volume, outv, 2 * sizeof(float));
 }
 
 void LimiterModule::_interface_proc() {
     ImGuiStyle& style = ImGui::GetStyle();
     float in_vol[2], out_vol[2];
+    
+    // read messages sent from process thread
+    while (true)
+    {
+        MessageQueue::read_handle_t handle = ui_queue.read();
+        if (!handle) break;
 
-    lock.lock();
-    float input_gain = _input_gain;
-    float output_gain = _output_gain;
-    float cutoff = _cutoff;
-    float decay = _decay;
-    float attack = _attack;
-    memcpy(in_vol, _in_volume, 2 * sizeof(float));
-    memcpy(out_vol, _out_volume, 2 * sizeof(float));
-    lock.unlock();
+        assert(handle.size() == sizeof(message_t));
+        message_t* msg = (message_t*) handle.data();
+
+        // update analytics state
+        if (msg->type == message_t::ReceiveAnalytics) {
+            ui_analytics = msg->analytics;
+            waiting = false;
+        }
+    }
+
+    memcpy(in_vol, ui_analytics.in_volume, 2 * sizeof(float));
+    memcpy(out_vol, ui_analytics.out_volume, 2 * sizeof(float));
 
     float width = ImGui::GetTextLineHeight() * 14.0f;
     ImGui::PushItemWidth(width);
@@ -196,25 +239,38 @@ void LimiterModule::_interface_proc() {
         ImGui::ProgressBar(out_vol[i], ImVec2(width, bar_height), "");
     }
 
-    ImGui::SliderFloat("##threshold", &cutoff, -20.0f, 0, "%.3f dB");
-    if (ImGui::IsItemClicked(ImGuiMouseButton_Middle)) cutoff = -1.0f;
-    ImGui::SliderFloat("##attack", &attack, 1.0f, 1000.0f, "%.3f ms");
-    if (ImGui::IsItemClicked(ImGuiMouseButton_Middle)) attack = 10.0f;
-    ImGui::SliderFloat("##decay", &decay, 1.0f, 1000.0f, "%.3f ms");
-    if (ImGui::IsItemClicked(ImGuiMouseButton_Middle)) decay = 500.0f;
-    ImGui::SliderFloat("##input_gain", &input_gain, -20.0f, 20.0f, "%.3f dB");
-    if (ImGui::IsItemClicked(ImGuiMouseButton_Middle)) input_gain = 0.0f;
-    ImGui::SliderFloat("##output_gain", &output_gain, -20.0f, 20.0f, "%.3f dB");
-    if (ImGui::IsItemClicked(ImGuiMouseButton_Middle)) output_gain = 0.0f;
+    ImGui::SliderFloat("##threshold", &ui_state.threshold, -20.0f, 0, "%.3f dB");
+    if (ImGui::IsItemClicked(ImGuiMouseButton_Middle)) ui_state.threshold = -1.0f;
+    ImGui::SliderFloat("##attack", &ui_state.attack, 1.0f, 1000.0f, "%.3f ms");
+    if (ImGui::IsItemClicked(ImGuiMouseButton_Middle)) ui_state.attack = 10.0f;
+    ImGui::SliderFloat("##decay", &ui_state.decay, 1.0f, 1000.0f, "%.3f ms");
+    if (ImGui::IsItemClicked(ImGuiMouseButton_Middle)) ui_state.decay = 500.0f;
+    ImGui::SliderFloat("##input_gain", &ui_state.input_gain, -20.0f, 20.0f, "%.3f dB");
+    if (ImGui::IsItemClicked(ImGuiMouseButton_Middle)) ui_state.input_gain = 0.0f;
+    ImGui::SliderFloat("##output_gain", &ui_state.output_gain, -20.0f, 20.0f, "%.3f dB");
+    if (ImGui::IsItemClicked(ImGuiMouseButton_Middle)) ui_state.output_gain = 0.0f;
 
     ImGui::EndGroup();
     ImGui::PopItemWidth();
+    
+    // send updated module state to audio thread,
+    // and request audio thread for analytics
+    {
+        message_t new_msg;
 
-    lock.lock();
-    _input_gain = input_gain;
-    _output_gain = output_gain;
-    _cutoff = cutoff;
-    _decay = decay;
-    _attack = attack;
-    lock.unlock();
+        if (!waiting) {
+            new_msg.type = message_t::RequestAnalytics;
+            if (process_queue.post(&new_msg, sizeof(new_msg))) {
+                dbg("WARNING: LimiterModule process queue is full!\n");
+            } else {
+                waiting = true;
+            }
+        }
+
+        new_msg.type = message_t::ModuleState;
+        new_msg.mod_state = ui_state;
+        if (process_queue.post(&new_msg, sizeof(new_msg))) {
+            dbg("WARNING: LimiterModule process queue is full!\n");
+        }
+    }
 }
--- src/modules/limiter.h
+++ src/modules/limiter.h
@@ -10,18 +10,38 @@ namespace audiomod
         void process(float** inputs, float* output, size_t num_inputs, size_t buffer_size, int sample_rate, int channel_count) override;
         void _interface_proc() override;
         
-        // DO NOT DO ANY REALTIME-SAFE OPERATIONS
-        // WHILE THE LOCK IS ACTIVE!!
-        SpinLock lock;
-        float _input_gain;
-        float _output_gain;
-        float _cutoff;
-        float _attack;
-        float _decay;
-        float _limit[2];
-        float _in_volume[2];
-        float _out_volume[2];
+        // keep two copies of the module state, one for the
+        // processing thread, and another for the ui thread.
+        struct module_state {
+            float input_gain,
+                  output_gain,
+                  threshold,
+                  attack,
+                  decay;
+        } process_state, ui_state;
 
+        struct analytics_t {
+            float in_volume[2];
+            float out_volume[2];
+        } process_analytics, ui_analytics;
+
+        struct message_t {
+            enum Type : uint8_t {
+                ModuleState, ReceiveAnalytics, RequestAnalytics
+            } type;
+
+            union {
+                module_state mod_state;
+                analytics_t analytics;
+            };
+        };
+
+        MessageQueue process_queue;
+        MessageQueue ui_queue;
+        bool waiting = false; // waiting for analytics response
+
+        float _limit[2];
+        
         static constexpr size_t BUFFER_SIZE = 1024;
         float _in_buf[2][BUFFER_SIZE];
         float _out_buf[2][BUFFER_SIZE];
